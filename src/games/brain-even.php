e80db61ab0fc223d972d676ff19f05f045f17b09
vladikKir <vlad@Vlad.localdomain>
2023-01-27 21:29:55 +0300

    Step 5

---
+++ src/games/brain-even.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Php\Project\Games\Brain\Even;
+
+use function Php\Project\Index\makeGame;
+
+function makeEven()
+{
+    $rule = 'Answer "yes" if the number is even, otherwise answer "no".';
+    function isEven($num)
+    {
+        return $num % 2 === 0;
+    }
+
+    function makeRound()
+    {
+    }
+
+    makeGame($rule, function () {
+        $question = random_int(1, 100);
+        $correctAnswer = isEven($question) ? 'yes' : 'no';
+
+        return [$question, $correctAnswer];
+    });
+}
---
+++ src/index.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Php\Project\Index;
+
+use function cli\line as line;
+use function cli\prompt;
+
+const ROUND_COUNT = 3;
+
+function makeGame($rule, $makeRound)
+{
+    line("Welcome to the Brain Game!");
+    $name = prompt("May I have your name?");
+    line("Hello, {$name}!");
+    line($rule);
+
+    for ($i = 0; $i < ROUND_COUNT; $i += 1) {
+        [$question, $correctAnswer] = $makeRound();
+
+        line("Question: {$question}");
+        $answer = prompt("Your answer: ");
+
+        if ($answer === $correctAnswer) {
+            line("Correct!");
+        } else {
+            line("{$answer} is wrong answer ;(. Correct answer was {$correctAnswer}.");
+            line("Let's try again, {$name}!");
+            return;
+        }
+    }
+    line("Congratulations, {$name}!");
+}
