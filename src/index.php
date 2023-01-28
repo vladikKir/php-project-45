46404287ad1b1a310c0c0bc9c48b8367f9106623
vladikKir <vlad@Vlad.localdomain>
2023-01-28 20:21:27 +0300

    fix greeting

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ const ROUND_COUNT = 3;
 
 function makeGame($rule, $makeRound)
 {
-    line("Welcome to the Brain Game!");
+    line("Welcome to the Brain Games!");
     $name = prompt("May I have your name?");
     line("Hello, {$name}!");
     line($rule);
